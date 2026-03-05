692cda12c69e3df757f86a4bd826998b823e5735
siddharth7actowiz <siddharth7.actowiz@gmail.com>
2026-03-05 15:55:05 +0530

    Add files via upload

---
+++ execute.bat
@@ -0,0 +1,6 @@
+start cmd /k python main.py 0 10000
+start cmd /k python main.py 10000 20000
+start cmd /k python main.py 20000 30000
+start cmd /k python main.py 30000 40000
+start cmd /k python main.py 40000 50000
+start cmd /k python main.py 50000 58933
---
+++ run.py
@@ -0,0 +1,13 @@
+import os
+
+from config import *
+
+files = os.listdir(DATA_DIR)
+files.sort()
+
+for s in range(start, end, files_batch):
+
+    last = min(s + files_batch, end)
+    command="start cmd /k python main.py " + str(s) + " " + str(last)
+    with open("execute.bat","a") as b:
+        b.write(command+'\n')
