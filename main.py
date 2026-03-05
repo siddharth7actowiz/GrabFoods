471460da678d823c8d1be518a8164260a0056d45
siddharth7actowiz <siddharth7.actowiz@gmail.com>
2026-03-05 12:32:33 +0530

    main file that executes all functions

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 import sys
 import time
-
 import config
-
 from utils import load_files
 from parser import parse_json
 from db import insert_into_database
 
 
 def main():
-
     start = int(sys.argv[1])
     end = int(sys.argv[2])
-
-    batch_data = []
 
     start_time = time.time()
 
@@ -23,22 +18,10 @@ def main():
 
         if not parsed:
             continue
-
-        batch_data.append(parsed)
-
-        # When batch full → insert
-        if len(batch_data) >= config.BATCH_SIZE:
-
-            insert_into_database(batch_data)
-
-            batch_data.clear()
-
-    # Insert remaining records
-    if batch_data:
-
-        insert_into_database(batch_data)
+        insert_into_database([parsed])
 
     print("Total pipeline time:", time.time() - start_time)
+
 
 
 if __name__ == "__main__":
