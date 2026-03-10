161f53f68676350e7fbb54e3edf0d6d3b4dc6e9c
siddharth7actowiz <siddharth7.actowiz@gmail.com>
2026-03-10 22:08:13 +0530

    Update modelvalidation.py

--- modelvalidation.py
+++ modelvalidation.py
@@ -1,73 +1,33 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import List, Optional
 
 
-class Offer(BaseModel):
-
-    Title: Optional[str]
-
-    SubTitle: Optional[str]
-
-
-class RestaurantDetails(BaseModel):
+class Rest(BaseModel):
 
     Restaurant_Name: Optional[str]
-
     Restaurant_ID: Optional[str]
-
     Branch_Name: Optional[str]
 
     Cuisine: Optional[str]
 
-    Tip: List[str] =[]
+    Tip: List = []
 
     Timezone: Optional[str]
 
-    ETA: Optional[int]
+    ETA: Optional[int]=None
 
-    DeliveryOptions: List[str]
+    DeliveryOptions: List = []
 
     Rating: Optional[float]
 
-    Is_Open: Optional[bool]
+    Is_Open: bool
 
     Currency_Code: Optional[str]
 
     Currency_Symbol: Optional[str]
 
-    Offers: List[Offer]=[]
+    Offers: List = []
 
     Timing_Everyday: Optional[str]
 
-
-class MenuItem(BaseModel):
-
-    Restaurant_ID: Optional[str]
-
-    Category_Name: Optional[str]
-
-    Item_ID: Optional[str]
-
-    Item_Name: Optional[str]
-
-    Item_Description: Optional[str]
-
-    Item_Price: Optional[float]
-
-    Item_Discounted_Price: Optional[float]
-
-    Item_Image_URL: Optional[list]
-
-    Item_Thumbnail_URL: Optional[list]
-
-    Item_Available: Optional[bool]
-
-    Is_Top_Seller: Optional[bool]
-
-
-class Rest(BaseModel):
-
-    Restaurant_Details: RestaurantDetails
-
-
-    Menu_Items: List[MenuItem]
+    Menu: Optional[str]
