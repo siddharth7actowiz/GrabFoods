e716114c28ea243737a5b8917f8b96f326357abb
siddharth7actowiz <siddharth7.actowiz@gmail.com>
2026-03-10 08:15:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,12 @@ PASSWORD="sid"
 HOST="localhost"
 PORT=3306
 DB="grabfood"
+files_batch=10000
+start=0
+end=len(os.listdir(DATA_DIR))
+BATCH_SIZE = 500
+tab1="pdp_restaurant_table"
+tab2="pdp_menu_items_table"
+total_threads=end//files_batch
 
 BATCH_SIZE = 500
